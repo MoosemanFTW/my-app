c053e89c38c9b6cd34ae859f08a9cb7705e16616
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2021-09-05 20:20:42 -0700

    save point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <h1>Todo list</h1>
+    <form>
+      <input type="checkbox" id="gym" name="gym" value="gym"/>
+      <label for="gym"> Go to the Gym</label><br/>
+      <input type="checkbox" id="laundry" name="laundry" value="laundry"/>
+      <label for="laundry"> Do my laundry</label><br/>
+      <input type="checkbox" id="food" name="food" value="food"/>
+      <label for="food"> Buy groceries</label><br/>
+      <input type="checkbox" id="homework" name="homework" value="homework"/>
+      <label for="homework"> Do my homework</label>
+    </form>
+    </div> 
+  ); 
 }
 
 export default App;
